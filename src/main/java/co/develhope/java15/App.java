9629953c2de823952a4246a87fa4fd54bafd7f12
aegroto <aegroto@protonmail.com>
2023-09-08 16:13:22 +0200

    file reading example

--- src/main/java/co/develhope/java15/App.java
+++ src/main/java/co/develhope/java15/App.java
@@ -1,7 +1,14 @@
 package co.develhope.java15;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.json.JSONObject;
 
@@ -15,16 +22,61 @@ public class App {
         JSONObject obj = new JSONObject(json);
         System.out.println(obj.getString("name"));
 
-        List<String> names = List.of("Emanuele", "Giuseppe", "Alessandro", "Maurizio");
+        System.out.println(" --- ");
 
-        System.out.println(names);
+        System.out.println("Using stream:");
+        printAllLinesUsingStream("names.txt");
 
-        names.stream()
-            .filter(App::startsWithE)
-            .forEach(System.out::println);
+        System.out.println("Using readAllLines and a for each loop:");
+        printAllLinesUsingReadAndFor("names.txt");
+
+        System.out.println("Using stream and filter with length() < 5:");
+        printFilteredUsingStream("names.txt", App::isShorterThan5);
+
+        System.out.println("Using stream and filter starting with 'S':");
+        printFilteredUsingStream("names.txt", name -> name.startsWith("S"));
+
+        System.out.println("Using stream and filter starting with 'F':");
+        printFilteredUsingStream("names.txt", name -> name.startsWith("F"));
+
     }
 
-    public static boolean startsWithE(String s) {
-        return s.startsWith("E");
+    static void printAllLinesUsingStream(String filePath) {
+        try {
+            Stream<String> lines = Files.lines(Path.of(filePath));
+            lines.forEach(System.out::println);
+            lines.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void printFilteredUsingStream(String filePath, Predicate<String> predicate) {
+        try {
+            Stream<String> lines = Files.lines(Path.of(filePath));
+            lines
+                .filter(predicate)
+                .forEach(System.out::println);
+            lines.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    static void printAllLinesUsingReadAndFor(String filePath) {
+        try {
+            List<String> lines = Files.readAllLines(Path.of(filePath));
+            for(String line : lines) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    static boolean isShorterThan5(String s) {
+        return s.length() < 5;
     }
 }
