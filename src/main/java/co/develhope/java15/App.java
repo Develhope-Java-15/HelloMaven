3e0f9d5cc7a9c21301ee0de98760800c4b90b8b4
aegroto <aegroto@protonmail.com>
2023-09-08 15:05:30 +0200

    stream example

--- src/main/java/co/develhope/java15/App.java
+++ src/main/java/co/develhope/java15/App.java
@@ -1,5 +1,8 @@
 package co.develhope.java15;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.json.JSONObject;
 
 /**
@@ -10,7 +13,18 @@ public class App {
     public static void main(String[] args) {
         String json = "{ \"name\": \"Lorenzo\", \"age\": 25 }";
         JSONObject obj = new JSONObject(json);
-
         System.out.println(obj.getString("name"));
+
+        List<String> names = List.of("Emanuele", "Giuseppe", "Alessandro", "Maurizio");
+
+        System.out.println(names);
+
+        names.stream()
+            .filter(App::startsWithE)
+            .forEach(System.out::println);
+    }
+
+    public static boolean startsWithE(String s) {
+        return s.startsWith("E");
     }
 }
